997b512f7503b555443d3a1eb522f0d1201b2018
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-03-22 22:25:07 -0700

    added to tag-route file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
+  through: {
+    model: Tag,
+  },
+  as: 'category_id'
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tags, {
+  through: {
+    model: ProductTag,
+  },
+  as: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  as: 'tag_id' 
+});
 
 module.exports = {
   Product,
